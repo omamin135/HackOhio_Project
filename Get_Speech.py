2fa22f1b278cbfaa20dec61ef5b9d2ca61c76031
GlazedDoughnuts03 <kondapaneni.4@buckeyemail.osu.edu>
2022-10-08 16:46:50 -0400

    yolo

--- Get_Speech.py
+++ Get_Speech.py
@@ -1,51 +1,47 @@
 import speech_recognition as sr
 import pyttsx3
-import time as t
+import time
 
-# Initialize the recognizer
-r = sr.Recognizer()
+class sttc:
 
-# Function to convert text to
-# speech
-def SpeakText(command):
-	
-	# Initialize the engine
-	engine = pyttsx3.init()
-	engine.say(command)
-	engine.runAndWait()
-	
-	
-# Loop infinitely for user to
-# speak
-def speechToText():
-    x = 1
-    
-while(1):
-	
-	# Exception handling to handle
-	# exceptions at the runtime
-	try:
-		
-		# use the microphone as source for input.
-		with sr.Microphone() as source2:
-			
-			# wait for a second to let the recognizer
-			# adjust the energy threshold based on
-			# the surrounding noise level
-			r.adjust_for_ambient_noise(source2, duration=0.2)
-			
-			#listens for the user's input
-			audio2 = r.record(source2, duration = 5)
-			
-			# Using google to recognize audio
-			MyText = r.recognize_google(audio2)
-			MyText = MyText.lower()
+    def stt():
+        # Initialize the recognizer
+        r = sr.Recognizer()
 
-			print("Did you say "+MyText)
-			SpeakText(MyText)
-			
-	except sr.RequestError as e:
-		print("Could not request results; {0}".format(e))
-		
-	except sr.UnknownValueError:
-		print("unknown error occured")
+        # Function to convert text to
+        # speech
+            
+            
+        # Loop infinitely for user to
+        # speak
+
+        t_end = time.time() + 5
+
+        while(time.time() < t_end):
+            
+            # Exception handling to handle
+            # exceptions at the runtime
+            try:
+                
+                # use the microphone as source for input.
+                with sr.Microphone() as source2:
+                    
+                    # wait for a second to let the recognizer
+                    # adjust the energy threshold based on
+                    # the surrounding noise level
+                    r.adjust_for_ambient_noise(source2, duration=0.2)
+                    
+                    #listens for the user's input
+                    audio2 = r.record(source2, duration = 5)
+                    
+                    # Using google to recognize audio
+                    MyText = r.recognize_google(audio2)
+                    MyText = MyText.lower()
+
+                    print("Did you say "+MyText)
+                    
+            except sr.RequestError as e:
+                print("Could not request results; {0}".format(e))
+                
+            except sr.UnknownValueError:
+                print("unknown error occured")
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+import Get_Speech
+import keyboard as k
 
-
-
-        
-
-
-    
-
-
-
+while (1):    
+    try:
+        if k.is_pressed('space'):
+            print("ending")
+            break
+    except:
+        while (True):
+            Get_Speech.sttc.stt()
 
 
